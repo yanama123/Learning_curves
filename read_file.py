011f3ffc7babef148cbfd9d59e6635f90ecdbcb4
Anjaneyulu Y <anji.anand211@gmail.com>
2019-11-19 18:07:43 +0530

    Add files via upload

--- read_file.py
+++ read_file.py
@@ -6,7 +6,7 @@ logging.basicConfig(level=logging.INFO)
 
 
 def read_and_parse_data(data):
-    logging.info('{} data is going to read and parse'.format(data))
+    logging.info('The data is going to read and parse here: ')
     final_data = []
     try:
         for line in data:
@@ -25,17 +25,8 @@ def read_and_parse_data(data):
 
                 line_group.append(temp[0])
                 final_data.append(line_group)
-                logging.info('data parsed and converted to list : {}'.format(final_data))
+        logging.info('The data parsed and converted to list : {}'.format(final_data))
     except Exception as e:
-        logging.error('Error occured during file parse: {}'.format(e))
+        logging.error('Error occurred during file parse: {}'.format(e))
 
     return final_data
-
-# pattern = r'.(.*AM) [a-zA-Z]+: cofigID:([0-9]+) configName:"([a-zA-Z]+)" configStatus:([a-zA-Z]+)'
-# pat = re.compile(pattern)
-# with open('a.log') as f:
-#     for line in f.readline():
-#         match = pat.match(line)
-#         if match:
-#             print("matching object is found")
-#             print("Date is : {}".format(match.group(1)))
